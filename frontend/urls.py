c136825a7abfecfd921f4086897b8527b56acef4
David Nanjila <nanjiladavid2@gmail.com>
2024-07-19 19:36:08 +0300

    create README.md file

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add_obituary/', views.add_obituary, name='add_obituary'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,35 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
 from backend.models import Obituary
+obituaries = Obituary.objects.all()
+
 
 def home(request):
-    return render(request, 'frontend/home.html')
-
-def add_obituary(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        date_of_birth = request.POST.get('date_of_birth')
-        date_of_death = request.POST.get('date_of_death')
-        content = request.POST.get('content')
-        author = request.POST.get('author')
-        submission_date = request.POST.get('submission_date')
-        slug = request.POST.get('slug')
-
-        # Create and save the Obituary object
-        obituary = Obituary(
-            name=name,
-            date_of_birth=date_of_birth,
-            date_of_death=date_of_death,
-            content=content,
-            author=author,
-            submission_date=submission_date,
-            slug=slug # Assuming you want to generate a slug from the name
-        )
-        obituary.save()
-
-        # Redirect to a success page or any other page
-        return redirect('home')  # Replace with the appropriate URL name
-
-    # If GET request or form is not valid, render the form template
-    return render(request, 'frontend/submit_obituary.html')
+      return render(request, 'frontend/home.html', {'obituaries': obituaries})
