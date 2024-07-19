2918df31f378c87dbb16d17305bc5bdfee2a220e
David Nanjila <nanjiladavid2@gmail.com>
2024-07-19 19:31:29 +0300

    fix

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import render, redirect
 from django.utils.text import slugify
 from django.utils import timezone
 
@@ -11,10 +12,36 @@ class Obituary(models.Model):
     submission_date = models.DateField(default=timezone.now)
     slug = models.SlugField(unique=True, max_length=100, null=False, blank=False)
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
-
     def __str__(self):
         return self.name
+
+def add_obituary(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        date_of_birth = request.POST.get('date_of_birth')
+        date_of_death = request.POST.get('date_of_death')
+        content = request.POST.get('content')
+        author = request.POST.get('author')
+        submission_date = request.POST.get('submission_date')
+
+        # Create and save the Obituary object
+        new_obituary = Obituary(
+            name=name,
+            date_of_birth=date_of_birth,
+            date_of_death=date_of_death,
+            content=content,
+            author=author,
+            submission_date=submission_date,
+            slug=slugify(name)  # Generate a slug from the name
+        )
+        new_obituary.save()
+
+        # Redirect to a success page or any other page
+        return redirect('home')  # Replace with the appropriate URL name
+
+    # If GET request or form is not valid, render the form template
+    return render(request, 'frontend/submit_obituary.html')
+
+def home(request):
+    obituaries = Obituary.objects.all()
+    return render(request, 'frontend/home.html', {'obituaries': obituaries})
